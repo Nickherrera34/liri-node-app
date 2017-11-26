7043cc29cc5ebc77074e44663a347849fd247c0a
Nick Herrera <nickherrera34@gmail.com>
2017-11-26 10:43:30 -0600

    commit

---
+++ assets/javascript/keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'LLR88uEKOaCoZf45nccroFWkW',
+  consumer_secret: '1kr4JXnTz4NxvDvwjUK1JzZwxXK77toGCPjwEiormhKstsc0CY',
+  access_token_key: '934094804580995073-zPQvOIE5p1qnGLGSTPJIwGAhh2JgSK2',
+  access_token_secret: 'GjqD6hdL6UdMMowbZxH20cTXE9W1eqvUTXzY3Rnw685Lh',
+}
+
+module.exports = twitterKeys;
