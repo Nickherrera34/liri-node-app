07ed7f82f482781f290aaa7cb1d49c4e65256d0c
Nick Herrera <nickherrera34@gmail.com>
2017-11-27 16:23:25 -0600

    add more

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -1,10 +1,25 @@
-console.log('this is loaded');
-
-var twitterKeys = {
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+// var OMDB = require('omdb')
+ 
+var twitterKeys = new Twitter({
   consumer_key: 'LLR88uEKOaCoZf45nccroFWkW',
   consumer_secret: '1kr4JXnTz4NxvDvwjUK1JzZwxXK77toGCPjwEiormhKstsc0CY',
   access_token_key: '934094804580995073-zPQvOIE5p1qnGLGSTPJIwGAhh2JgSK2',
   access_token_secret: 'GjqD6hdL6UdMMowbZxH20cTXE9W1eqvUTXzY3Rnw685Lh',
+});
+
+var spotifyKeys = new Spotify({
+	id: 'fdb6c430248f48ceb811876daee311f2',
+  	secret: '16b4cd173e4a42cbb265da9143150338'
+});
+
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys,
+	// omdbKeys: omdbKeys
 }
 
-module.exports = twitterKeys;
+// var omdbKeys = new OMDB({
+// 	key: "fbede96a"
+// });
--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,24 +1,78 @@
-const http = require('http');
-const fs = require ('fs');
-
-const hostname = ('127.0.0.1');
-const port = 3000;
-
-fs.readFile('../../index.html', (err, html) => {
-	if (err){
-		throw err;
-	};
-
-	const server = http.createServer((req, res) =>{
-		res.statusCode = 200;
-		res.setHeader('content-type', 'text/html');
-		res.write(html);
-		res.end();
-	});
-
-	server.listen(port, hostname, () => {
-		console.log('Server Started on port ' +port);
-	});
+var client = require("./keys.js");
+var request = require('request');
 
 
-})
+var method = process.argv[2];
+var argTwo = process.argv.slice(3).join(" ");
+
+switch(method){
+
+	case "my-tweets":
+		client.twitterKeys.get('statuses/user_timeline', function(error, tweets, response) {
+		  if(error) throw error;
+
+		  for(var i = 0; i < tweets.length; i++){
+		  	console.log(tweets[i].text);
+		  }
+		 
+		});
+		break;
+
+	case "spotify-this-song":
+			client.spotifyKeys.search({ type: 'track', query: argTwo, limit: 1 }, function(err, data) {
+			  if (err) {
+			    return console.log('Error occurred: ' + err);
+			  }
+			 // console.log(data.tracks.items[0].artists[0]);
+			 for(var i = 0; i < data.tracks.items[0].artists.length; i++){
+			 	console.log("Artists: " + data.tracks.items[0].artists[i].name);
+
+			 }
+			console.log("Song Name: ", data.tracks.items[0].name)
+			console.log("Song Link: ", data.tracks.items[0].album.href)
+			console.log("Album Name: ", data.tracks.items[0].album.name)
+		});
+		break;
+
+	case "movie-this":
+
+		if (argTwo === undefined){
+			argTwo = "Mr+Nobody";
+		};
+
+		var movieURL = "http://www.omdbapi.com/?t=" + argTwo +"&apikey=fbede96a";
+		// console.log(movieURL)
+
+		request(movieURL, function(error, response, body){
+				if (argTwo === undefined) {
+				    argTwo = 'Mr Nobody';
+				  }
+
+				 request(movieURL, function(error, response, body) {
+				    if (!error && response.statusCode == 200) {
+				      var data = [];
+				      var jsonData = JSON.parse(body);
+				      // console.log(jsonData)
+				      console.log("Title: " + jsonData.Title);
+				      console.log('Year: ' + jsonData.Year);
+				      console.log('Rated: ' + jsonData.Rated);
+				      console.log('IMDB Rating: ' + jsonData.imdbRating);
+				      console.log('Country: ' + jsonData.Country);
+				      console.log('Language: ' + jsonData.Language);
+				      console.log('Plot: ' + jsonData.Plot);
+				      console.log('Actors: ' + jsonData.Actors);
+				      console.log('Rotten Tomatoes Rating: ' + jsonData.Ratings[1].Value);
+				  }
+
+			});
+		
+		});
+
+		break;
+
+		case "do-what-it-says":
+
+		break;
+
+};
+
